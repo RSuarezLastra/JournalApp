fbe779306684f63a5760183b311d909dfa7947f2
RSLastra <raul.slastra@gmail.com>
2024-04-26 20:22:06 -0600

    authlayout and loginPage

--- src/auth/pages/LoginPage.jsx
+++ src/auth/pages/LoginPage.jsx
@@ -1,7 +1,59 @@
+import { Link as RouterLink } from "react-router-dom"
+import { Google } from "@mui/icons-material"
+import { Button, Grid, Link, TextField, Typography } from "@mui/material"
+import { AuthLayout } from "../layout/AuthLayout"
 
 
 export const LoginPage = () => {
     return (
-        <div>LoginPage</div>
+
+        <AuthLayout title='login'>
+            <form>
+                <Grid container>
+                    <Grid item xs={12} sx={{ mt: 2 }} >
+                        <TextField
+                            label="email"
+                            type="email"
+                            placeholder="raul.s@gmail.com"
+                            fullWidth
+                        />
+                    </Grid>
+
+                    <Grid item xs={12} sx={{ mt: 2 }} >
+                        <TextField
+                            label="password"
+                            type="password"
+                            fullWidth
+                        />
+                    </Grid>
+                </Grid>
+
+                <Grid container spacing={2} sx={{ my: 2 }}>
+                    <Grid item xs={12} sm={6}>
+                        <Button
+                            variant="contained"
+                            fullWidth>
+                            Login
+                        </Button>
+                    </Grid>
+
+                    <Grid item xs={12} sm={6}>
+                        <Button
+                            variant="contained"
+                            fullWidth>
+                            <Google />
+                            <Typography sx={{ ml: 1 }}>Google</Typography>
+                        </Button>
+                    </Grid>
+
+
+                </Grid>
+                <Grid container direction='row' justifyContent='end'>
+                    <Link component={RouterLink} color='inherit' to='/auth/register'>
+                        Crear una cuenta
+                    </Link>
+                </Grid>
+            </form>
+        </AuthLayout>
     )
 }
