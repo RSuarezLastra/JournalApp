6b764001aaf1819120b673ad8672a2e441bce6d3
RSLastra <raul.slastra@gmail.com>
2024-05-14 19:41:34 -0600

    feat: jounalSlice && addNewEmptyNote

--- src/journal/pages/JournalPage.jsx
+++ src/journal/pages/JournalPage.jsx
@@ -1,16 +1,31 @@
+import { useDispatch, useSelector } from "react-redux"
 import { JournalLayout } from "../layout/JournalLayout"
 import { IconButton, Typography } from "@mui/material"
 import { NothingSelectedView, NoteView } from "../views"
 import { AddOutlined } from "@mui/icons-material"
+import { startNewNote } from "../../store/journal"
 
 export const JournalPage = () => {
+
+    const dispatch = useDispatch();
+    const { isSaving, active } = useSelector(state => state.journal)
+
+    const onClickNewNote = () => {
+        dispatch(startNewNote());
+    }
+
     return (
         <JournalLayout>
 
-            {/* <NoteView/> */}
-            <NothingSelectedView />
+            {
+                (!!active)
+                    ? <NoteView />
+                    : <NothingSelectedView />
+            }
 
             <IconButton
+                onClick={onClickNewNote}
+                disabled={isSaving}
                 size='large'
                 sx={{
                     color: 'white',
@@ -20,7 +35,7 @@ export const JournalPage = () => {
                     right: 50,
                     bottom: 50
                 }}>
-                <AddOutlined sx={{ fontSize: 30}} />
+                <AddOutlined sx={{ fontSize: 30 }} />
 
             </IconButton>
 
