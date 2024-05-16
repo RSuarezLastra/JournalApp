d993f64c48b38c4fbe7c30918c1d640faf6ffa0a
RSLastra <raul.slastra@gmail.com>
2024-05-15 19:58:44 -0600

    feat: onClickNote

--- src/journal/components/NoteList.jsx
+++ src/journal/components/NoteList.jsx
@@ -1,19 +1,27 @@
+import { useDispatch } from "react-redux";
 import { useMemo } from "react";
 import { TurnedInNot } from "@mui/icons-material";
 import { ListItem, ListItemButton, ListItemIcon, ListItemText, Grid } from "@mui/material"
+import { setActiveNote } from "../../store/journal";
 
 
-export const NoteList = ({ id, title, body }) => {
+export const NoteList = ({ id, title, body, date, imageUrls = [] }) => {
+
+    const dispatch = useDispatch();
 
     const newTitle = useMemo(() => {
         return title.length > 17
-            ? title.substring(0, 17)
+            ? title.substring(0, 17) + '...'
             : title
-    }, [title])
+    }, [title]);
+
+    const onClickNote = () => {
+        dispatch(setActiveNote({id, title, body, date, imageUrls }))
+    }
 
     return (
         <ListItem disablePadding>
-            <ListItemButton>
+            <ListItemButton onClick={onClickNote} >
                 <ListItemIcon>
                     <TurnedInNot />
                 </ListItemIcon>
