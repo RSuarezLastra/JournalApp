166befaa808228bdd948572f30b19ee59ee0279b
RSLastra <raul.slastra@gmail.com>
2024-05-16 20:44:22 -0600

    feat: startSaveNote

--- src/journal/views/NoteView.jsx
+++ src/journal/views/NoteView.jsx
@@ -1,12 +1,13 @@
-import { useMemo } from "react";
-import { useSelector } from "react-redux";
+import { useMemo, useEffect } from "react";
+import { useDispatch, useSelector } from "react-redux";
 import { useForm } from "../../hooks";
 import { Grid, Typography, Button, TextField } from "@mui/material"
 import { SaveOutlined } from "@mui/icons-material"
 import { ImageGallery } from "../components"
+import { setActiveNote, startSaveNote } from "../../store/journal";
 
 export const NoteView = () => {
-
+    const dispatch = useDispatch();
     const { active: note } = useSelector(state => state.journal);
 
     const { body, title, date, onInputChange, formState } = useForm(note);
@@ -15,6 +16,14 @@ export const NoteView = () => {
         const newDate = new Date(date);
         return newDate.toLocaleDateString('es')
     }, [date])
+
+    useEffect(() => {
+        dispatch(setActiveNote(formState))
+    }, [formState]);
+
+    const onSaveNote = () => {
+        dispatch(startSaveNote());
+    }
 
     return (
         <Grid container
@@ -28,7 +37,9 @@ export const NoteView = () => {
                 <Typography fontSize={24} fontWeight='light'>{dateString}</Typography>
             </Grid>
             <Grid item>
-                <Button sx={{ p: 1 }}>
+                <Button
+                    onClick={onSaveNote}
+                    sx={{ p: 1 }}>
                     <SaveOutlined sx={{ fontSize: 25, mr: 1 }} />
                     Guardar
                 </Button>
